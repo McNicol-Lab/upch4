331c75bd8cfbbe2b845d0d28718593ad725a8aee
Gavin McNicol <gmcnicol@stanford.edu>
2021-01-26 17:27:56 -0600

    updated timeseries figure code to output both gcp and upscaling predictions

--- code/upscaling_gcp_timeseries.R
+++ code/upscaling_gcp_timeseries.R
@@ -27,31 +27,41 @@ upscaling[["monthly_sum_95ci.csv"]] %>%
   my_theme
 
 # read in gcp sums 
-gcp <- read_csv("results/final_product_eval/gcp_ch4_model_sum_2000_2017_indiv.csv") %>% 
-  dplyr::select(-X1, - units) %>% # units refer to original product, not sums 
-  mutate(Tg_month = flux,
-         model = sub("\\_.*", "", model), # model name from file name
-         timestep = rep(1:156, n()/156)) %>% 
-  dplyr::select(model, timestep, Tg_month)
+gcp <- read_csv("results/final_product_eval/gcp_monthly_sum.csv") %>% 
+  mutate(type = "gcp")
 
 # check on GCP global sums 
 gcp %>% 
   group_by(model) %>% 
   filter(timestep %in% c(1:12)) %>% 
-  summarize(sum = sum(Tg_month))
+  summarize(sum = sum(tg_month))
 
 # merge with upscaling data
 all <- upscaling[["member_monthly_sum_95ci.csv"]] %>% 
-  mutate(model = paste("upscaling", member, sep =  "_")) %>% 
-  dplyr::select(model, timestep, Tg_month = mean) %>% 
-  bind_rows(gcp)
+  mutate(model = paste("m", member, sep =  "_"),
+         type = "upscaling") %>% 
+  dplyr::select(type, model, timestep, tg_month = mean, ci) %>% 
+  bind_rows(gcp) %>% 
+  mutate(ci = ifelse(is.na(ci), 0, ci), 
+         Year = ceiling(timestep/12) + 2000) %>% 
+  group_by(type, model, Year) %>% 
+  mutate(month = 1:n(),
+         dec_year = Year + month/12)
 
 all %>% 
-  # mutate(plot_group = ifelse(member %in% c(1,2,3,4), "CRU-TS", "MERRA2")) %>% 
-  ggplot(aes(timestep, Tg_month, color = factor(model))) + 
-  # geom_ribbon(aes(ymin = mean + ci, ymax = mean - ci), fill = "light grey", color = 'light grey')  +
+  ggplot(aes(dec_year, tg_month, color = factor(model), alpha = factor(model))) + 
   geom_line() +
-  # facet_wrap(~plot_group, ncol = 1) +
+  # geom_ribbon(aes(ymin = tg_month + ci, ymax = tg_month - ci, fill = factor(model)))  +
+  # facet_wrap(~type, ncol = 1) +
+  scale_y_continuous(limits = c(0, 30)) +
+  scale_x_continuous(limits = c(2001, 2015)) +
+  scale_color_manual(values = c(rep("grey", 9), rep("red", 4), rep("blue", 4), rep("grey", 4))) +
+  scale_alpha_manual(values = c(rep(0.4, 9), rep(0.8, 8), rep(0.4, 4))) +
+  # scale_fill_manual(values = c(rep(NA, 9), rep("red", 4), rep("blue", 4), rep(NA, 4))) + 
+  labs(x = "Year", y = expression( atop( "Wetland Emissions", paste("(Tg "*CH[4]*" month"^{-1}*")") ) ) ) +
   my_theme
-  
+ggsave("results/final_product_eval/upscaling_gcp_2001_2015_timeseries.png",
+       width = 24, height = 20, units = c("cm"), dpi = 300)
+
+
 
