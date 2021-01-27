d12a44650b699f2842f78ca205a92caae79381d9
Gavin McNicol <gmcnicol@stanford.edu>
2021-01-27 16:35:05 -0600

    added source for ggplot theme to bootstrap-sums-uncertainties, and added code to output the overall average emissions (pooled across all members and years) and the overall average emissions for each member (pooled across years)

--- code/bootstrap-sums-uncertainties.R
+++ code/bootstrap-sums-uncertainties.R
@@ -6,16 +6,7 @@ rm(list=ls())
 library(tidyverse)
 
 # ggplot theme
-mytheme <- theme_bw() +
-  theme(panel.border = element_blank(),
-        axis.title=element_text(size=14), axis.text=element_text(size=14),
-        panel.grid.major = element_blank(),
-        panel.grid.minor = element_blank(),
-        axis.line = element_line(colour = "black"),
-        legend.position = "bottom",
-        strip.text = element_text(face="bold", size=10),
-        strip.background = element_rect(fill='white', colour='white',size=1),
-        plot.margin=unit(c(1,2,1.5,1.2),"cm"))
+source("code/ggplot_theme.R")
 
 loc <- "/Volumes/LACIE SHARE/Stanford CH4/June 2020 Upscaling/Predictions/Gridded/individual_bootstrap/"
 setwd(loc)
@@ -55,8 +46,36 @@ all_data %>%
   group_by(Year) %>% 
   summarize(mean = mean(TgCH4year),
             sd = sd(TgCH4year),
-            ci = sd*1.96) %>% 
+            ci = sd*1.96) 
   write.csv("results/final_product_eval/annual_sum_95ci.csv",
+            row.names = F)
+  
+# overall member-average and uncertainty
+all_data %>% 
+    dplyr::select(bootstrap = b, member = m, timestep = t, TgCH4month = sum_TgCH4month) %>% 
+    mutate(Year = 2000+ceiling(timestep/12)) %>% 
+    group_by(member, Year, bootstrap) %>% 
+    summarize(TgCH4year = sum(TgCH4month),
+              length = n()) %>% 
+    group_by(member) %>% 
+    summarize(mean = mean(TgCH4year),
+              sd = sd(TgCH4year),
+              ci = sd*1.96) %>% 
+  write.csv("results/final_product_eval/member_annual_sum_95ci.csv",
+            row.names = F)
+
+# overall average and uncertainty
+all_data %>% 
+  dplyr::select(bootstrap = b, member = m, timestep = t, TgCH4month = sum_TgCH4month) %>% 
+  mutate(Year = 2000+ceiling(timestep/12)) %>% 
+  group_by(member, Year, bootstrap) %>% 
+  summarize(TgCH4year = sum(TgCH4month),
+            length = n()) %>% 
+  ungroup() %>% 
+  summarize(mean = mean(TgCH4year),
+            sd = sd(TgCH4year),
+            ci = sd*1.96) %>% 
+  write.csv("results/final_product_eval/overall_annual_sum_95ci.csv",
             row.names = F)
 
 # monthly overall member-average global sum and confidence intervals
@@ -65,7 +84,7 @@ all_data %>%
   group_by(timestep) %>% 
   summarize(mean = mean(TgCH4month),
             sd = sd(TgCH4month),
-            ci = sd*1.96)%>% 
+            ci = sd*1.96) 
   write.csv("results/final_product_eval/monthly_sum_95ci.csv",
             row.names = F)
 
@@ -75,32 +94,10 @@ all_data %>%
   group_by(member, timestep) %>% 
   summarize(mean = mean(TgCH4month),
             sd = sd(TgCH4month),
-            ci = sd*1.96) %>% 
+            ci = sd*1.96) 
   write.csv("results/final_product_eval/member_monthly_sum_95ci.csv",
             row.names = F)
 
+
   
 
-data[[1]] %>% 
-  dplyr::select(bootstrap = b, member = m, timestep = t, TgCH4month = sum_TgCH4month) %>% 
-  mutate(Year = 2000+ceiling(timestep/12)) %>% 
-  group_by(bootstrap, Year) %>% 
-  mutate(cum_TgCH4year = cumsum(TgCH4month)) %>% 
-  summarize(annual_cum_TgCH4year = max(cum_TgCH4year)) %>% 
-  group_by(Year) %>% 
-  summarize(sd = sd(annual_cum_TgCH4year))
-  summarize()
-  summarize(TgCH4year = sum(TgCH4month),
-            length = n()) %>% 
-  group_by(Year) %>% 
-  summarize(TgCH4year_mean = mean(TgCH4year),
-            TgCH4year_ci = sd(TgCH4year)*1.96)
-  
-  
-data[[1]] %>% 
-  dplyr::select(bootstrap = b, member = m, timestep = t, TgCH4month = sum_TgCH4month) %>% 
-  group_by(timestep) %>% 
-  summarize(TgCH4month = mean(TgCH4month)) %>% 
-  mutate(Year = 2000+ceiling(timestep/12)) %>% 
-  group_by(Year) %>% 
-  summarize(sum = sum(TgCH4month))
