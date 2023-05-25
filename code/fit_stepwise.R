4947029fbd94379e7a9f0406c9143ae5d33528b8
Gavin McNicol <gmcnicol@uic.edu>
2023-05-25 00:19:33 +0000

    completed code clean-up up to step-wise ffs

---
+++ code/fit_stepwise.R
@@ -0,0 +1,53 @@
+fit_stepwise <- function(feat_single){
+  
+  train_list <- list()
+  
+  for(i in 1:length(train_feat)){
+    
+    train_list[[i]] <- list(train_feat[[i]], train_label[[i]], feat_single, feat_best, validate_data[[i]])
+    names(train_list[[i]]) <- c("train_feat", "train_label", "feat_single", "feat_best", "validate_data")
+  }
+  
+  
+  fit_feat <- function(train_list){
+    
+    # initialize features
+    num.trees.rf <- ifelse(feat_initial < 5, 50, 100)
+    
+    ## Create tune-grid
+    tgrid <- expand.grid(
+      mtry = c(2),
+      splitrule = "variance", 
+      min.node.size = c(50)
+    )
+    
+    ## Create trainControl object
+    myControl <- trainControl(
+      method = "none",
+      classProbs = FALSE,
+      allowParallel = TRUE
+    )
+    
+    ## train rf on folds 
+    rf_model <- train(
+      x = train_list$train_feat[,c(feat_single, feat_best)],
+      y = train_list$train_label,
+      num.trees = num.trees.rf,
+      method = 'ranger',
+      trControl = myControl,
+      tuneGrid = tgrid,
+      metric = "MAE"
+    )
+    
+    
+    # get all hold out predictions
+    rf.pred <- train_list$validate_data %>%
+      mutate(FCH4P = predict(rf_model, .),
+             index = 1:n())
+    
+  }
+  
+  rf.pred.inner <- lapply(train_list, fit_feat)
+  rf.pred.all <<- bind_rows(rf.pred.inner) %>% dplyr::select(Cluster, feat_single, FCH4, FCH4P)
+  
+}
---
+++ code/ggplot_theme.R
@@ -0,0 +1,9 @@
+my_theme <- theme_bw() +
+  theme(panel.border = element_blank(), 
+        axis.title=element_text(size=14), axis.text=element_text(size=14),
+        panel.grid.minor = element_blank(), 
+        axis.line = element_line(colour = "black"),
+        legend.position = "bottom",
+        strip.text = element_text(face="bold", size=10),
+        strip.background = element_rect(fill='white', colour='white',linewidth = 1),
+        plot.margin=unit(c(1,2,1.5,1.2),"cm"))
